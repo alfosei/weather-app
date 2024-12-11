1a164517ab41372c82bb33bd9d3ab6de92761e3a
Alfred Osei <alfosei1234@gmail.com>
2024-12-11 09:53:34 +0000

    Enchance UI with weather icons, loading spinner, and background color changes

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 // Fetch Weather Data
 async function getWeather(city){
     try {
+        //Show the spinner
+        document.getElementById("loading-spinner").style.display = "block";
+
         //Construct the full API URL
         const url = `${baseUrl}?q=${city}&appid=${apiKey}&units=metric`;
 
@@ -13,18 +16,19 @@ async function getWeather(city){
 
         //Handle errors (such as invalid city)
         if(!response.ok){
-            const errorMessage = await response.text();
-            console.error("Error Response:", errorMessage);
             throw new Error("City not found!");
         }
 
         //Parse the response to JSON
         const data =  await response.json();
+        document.getElementById("loading-spinner").style.display = "none";
         return data;
     } catch (error){
         console.error("Error fetching weather data", error);
         alert(error.message);
+        document.getElementById("loading-spinner").style.display = "none";
     }
+
 }
 
 // Get References to DOM elements
@@ -36,6 +40,23 @@ function updateWeatherUI(data){
     document.getElementById("city-name").textContent = data.name;
     document.getElementById("temperature").textContent = `Temperature: ${data.main.temp} °C`;
     document.getElementById("condition").textContent = `Condition: ${data.weather[0].description}`;
+
+    // Add icon to the UI
+    const iconCode = data.weather[0].icon; // Get icon code
+    const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
+    document.getElementById("weather-icon").src = iconUrl;
+
+    //Change background color based on weather condition
+    const condition = data.weather[0].main.toLowerCase();
+    if(condition === "clear"){
+        document.body.style.backgroundColor = "#87CEEB";
+    } else if (condition === "clouds"){
+        document.body.style.backgroundColor = "#B0C4DE";
+    } else if (condition === "rain"){
+        document.body.style.backgroundColor = "#4682B4";
+    } else {
+        document.body.style.backgroundColor = "#f2e5d7";
+    }
 }
 
 // Add Event Listeners to Search Button
